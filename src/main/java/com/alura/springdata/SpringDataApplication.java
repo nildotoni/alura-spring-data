bbd837aac11d8e43575aa736ec05a85d4ea472e4
Nildo Silva <nildotoni@gmail.com>
2021-04-06 09:37:09 -0300

    Criado o repository e testado a inclusao de cargo com sucesso

--- src/main/java/com/alura/springdata/SpringDataApplication.java
+++ src/main/java/com/alura/springdata/SpringDataApplication.java
@@ -1,13 +1,31 @@
 package com.alura.springdata;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringDataApplication {
+import com.alura.springdata.orm.Cargo;
+import com.alura.springdata.repository.CargoRepository;
 
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner{
+
+	private final CargoRepository repository;
+	
+	public SpringDataApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+			Cargo cargo = new Cargo();
+			cargo.setDescricao("Desenvolvedor de software");
+			repository.save(cargo);
 	}
 
 }
---
+++ src/main/java/com/alura/springdata/repository/CargoRepository.java
@@ -0,0 +1,13 @@
+package com.alura.springdata.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alura.springdata.orm.Cargo;
+
+@Repository
+public interface CargoRepository extends CrudRepository<Cargo, Integer>{
+	
+	
+	
+}
